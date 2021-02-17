7bf549b5809c7f93ba35da8a0a4871e6ff6b2378
AlutkaG <alagerlach@interia.pl>
2021-02-17 11:45:40 +0100

    Added Router and Navbar module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+const newHistory = createBrowserHistory();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router history={newHistory}>
+			<div className='App'>
+				<Navbar />
+				{/*<Switch>
+					<Route exact path='/'>
+						<Redirect to='home' />
+					</Route>
+					<Route path='/home' component={Navbar} />
+        </Switch>*/}
+			</div>
+		</Router>
+	);
 }
 
 export default App;
