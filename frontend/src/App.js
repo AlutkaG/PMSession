f6ba935098bc3dd7d58d6a87cf6905764ce78940
AlutkaG <alagerlach@interia.pl>
2021-02-19 00:47:35 +0100

    Clearing the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import AboutConference from "./Components/AboutConference/AboutConference";
 import Statistics from "./Components/Statistics/Statistics";
 import Thoughts from "./Components/Thoughts/Thoughts";
 import TopWave from "./Components/TopWave/TopWave";
-import { Router, Switch, Route } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 const newHistory = createBrowserHistory();
