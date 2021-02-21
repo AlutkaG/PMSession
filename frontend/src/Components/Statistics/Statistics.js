29978c387f223a4f7cb8d033b7ef9bd062772195
AlutkaG <alagerlach@interia.pl>
2021-02-21 22:25:13 +0100

    Added responsive styles

--- frontend/src/Components/Statistics/Statistics.js
+++ frontend/src/Components/Statistics/Statistics.js
@@ -6,9 +6,62 @@ const Statistics = () => {
 			<div className='grey-circle2' />
 			<div style={{ marginTop: "-900px" }}>
 				<div className='stat-header'>Nasze statystyki</div>
-				<div className='stat-row1'>
+				<div className='stat-full-screen'>
+					<div className='stat-row1'>
+						<div className='stat-box'>
+							<div className='stat-number' style={{ marginTop: "53px" }}>
+								8
+							</div>
+							<div className='stat-text'>edycji</div>
+						</div>
+						<div className='stat-box'>
+							<div className='stat-number yellow' style={{ marginTop: "38px" }}>
+								47
+							</div>
+							<div className='stat-text yellow' style={{ marginTop: "15px" }}>
+								prelekcji
+							</div>
+						</div>
+						<div className='stat-box'>
+							<div className='stat-number' style={{ marginTop: "38px" }}>
+								134
+							</div>
+							<div className='stat-text' style={{ marginTop: "15px" }}>
+								prelegentów
+							</div>
+						</div>
+					</div>
+					<div className='stat-row2'>
+						<div className='stat-box'>
+							<div className='stat-number' style={{ marginTop: "47px" }}>
+								974
+							</div>
+							<div className='stat-text' style={{ marginTop: "18px" }}>
+								uczestników
+							</div>
+						</div>
+						<div className='stat-box'>
+							<div className='stat-number yellow' style={{ marginTop: "47px" }}>
+								69
+							</div>
+							<div className='stat-text yellow' style={{ marginTop: "18px" }}>
+								warsztatów
+							</div>
+						</div>
+						<div className='stat-box'>
+							<div className='stat-number' style={{ marginTop: "47px" }}>
+								8
+							</div>
+							<div className='stat-text' style={{ marginTop: "18px" }}>
+								symulacji
+							</div>
+						</div>
+					</div>
+				</div>
+				{/* mobile screen */}
+				<div className='stat-mobile-screen'>
 					<div className='stat-box'>
-						<div className='stat-number' style={{ marginTop: "53px" }}>
+						<div className='stat-number' style={{ marginTop: "30px" }}>
 							8
 						</div>
 						<div className='stat-text'>edycji</div>
@@ -17,43 +70,32 @@ const Statistics = () => {
 						<div className='stat-number yellow' style={{ marginTop: "38px" }}>
 							47
 						</div>
-						<div className='stat-text yellow' style={{ marginTop: "15px" }}>
-							prelekcji
-						</div>
+						<div className='stat-text yellow'>prelekcji</div>
 					</div>
 					<div className='stat-box'>
 						<div className='stat-number' style={{ marginTop: "38px" }}>
 							134
 						</div>
-						<div className='stat-text' style={{ marginTop: "15px" }}>
-							prelegentów
-						</div>
+						<div className='stat-text'>prelegentów</div>
 					</div>
-				</div>
-				<div className='stat-row2'>
+
 					<div className='stat-box'>
 						<div className='stat-number' style={{ marginTop: "47px" }}>
 							974
 						</div>
-						<div className='stat-text' style={{ marginTop: "18px" }}>
-							uczestników
-						</div>
+						<div className='stat-text'>uczestników</div>
 					</div>
 					<div className='stat-box'>
 						<div className='stat-number yellow' style={{ marginTop: "47px" }}>
 							69
 						</div>
-						<div className='stat-text yellow' style={{ marginTop: "18px" }}>
-							warsztatów
-						</div>
+						<div className='stat-text yellow'>warsztatów</div>
 					</div>
 					<div className='stat-box'>
 						<div className='stat-number' style={{ marginTop: "47px" }}>
 							8
 						</div>
-						<div className='stat-text' style={{ marginTop: "18px" }}>
-							symulacji
-						</div>
+						<div className='stat-text'>symulacji</div>
 					</div>
 				</div>
 			</div>
