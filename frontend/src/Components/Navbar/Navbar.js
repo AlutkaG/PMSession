648ea16eb681155d6f21f1254b5df155a0748a3c
AlutkaG <alagerlach@interia.pl>
2021-02-21 11:20:53 +0100

    Added icon menu, methods for side navbar and responsive styles

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,12 +1,20 @@
 import { HashLink as Link } from "react-router-hash-link";
 import logo from "../../Assets/logoPMSession.png";
+import { IoIosMenu } from "react-icons/io";
 
 import "./Navbar.css";
 
-const Navbar = () => {
+const Navbar = (props) => {
+	const handleOpen = () => {
+		props.openClickHandler();
+	};
+
 	return (
 		<div className='top-nav'>
 			<div className='top-nav-navi'>
+				<button className='icon-menu' onClick={(e) => handleOpen()}>
+					<IoIosMenu size='50px' color='#262626' />
+				</button>
 				<div>
 					<Link to='#'>
 						<img src={logo} alt='Logo' height='100px' />
