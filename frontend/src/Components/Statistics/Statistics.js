baaacfdb65606f630aa7265019386a1b7f9ca23b
AlutkaG <alagerlach@interia.pl>
2021-02-22 18:27:47 +0100

    added grid

--- frontend/src/Components/Statistics/Statistics.js
+++ frontend/src/Components/Statistics/Statistics.js
@@ -10,114 +10,30 @@ const Statistics = () => {
 					<div className='stat-full-screen'>
 						<div className='stat-row1'>
 							<div className='stat-box'>
-								<div className='stat-number' style={{ marginTop: "53px" }}>
-									8
-								</div>
-								<div
-									className='stat-text'
-									style={
-										window.matchMedia(
-											"(min-width:501px) and (max-width:1000px)"
-										) === true
-											? { marginTop: "-60px" }
-											: { marginTop: "0px" }
-									}
-								>
-									edycji
-								</div>
+								<div className='stat-number'>8</div>
+								<div className='stat-text'>edycji</div>
 							</div>
 							<div className='stat-box'>
-								<div
-									className='stat-number yellow'
-									style={{ marginTop: "38px" }}
-								>
-									47
-								</div>
-								<div
-									className='stat-text yellow'
-									style={
-										window.matchMedia(
-											"(min-width:501px) and (max-width:1000px)"
-										) === true
-											? { marginTop: "-50px" }
-											: { marginTop: "15px" }
-									}
-								>
-									prelekcji
-								</div>
+								<div className='stat-number yellow'>47</div>
+								<div className='stat-text yellow'>prelekcji</div>
 							</div>
 							<div className='stat-box'>
-								<div className='stat-number' style={{ marginTop: "38px" }}>
-									134
-								</div>
-								<div
-									className='stat-text'
-									style={
-										window.matchMedia(
-											"(min-width:501px) and (max-width:1000px)"
-										) === true
-											? { marginTop: "-50px" }
-											: { marginTop: "15px" }
-									}
-								>
-									prelegentów
-								</div>
+								<div className='stat-number'>134</div>
+								<div className='stat-text'>prelegentów</div>
 							</div>
 						</div>
 						<div className='stat-row2'>
 							<div className='stat-box'>
-								<div className='stat-number' style={{ marginTop: "47px" }}>
-									974
-								</div>
-								<div
-									className='stat-text'
-									style={
-										window.matchMedia(
-											"(min-width:501px) and (max-width:1000px)"
-										) === true
-											? { marginTop: "-50px" }
-											: { marginTop: "18px" }
-									}
-								>
-									uczestników
-								</div>
+								<div className='stat-number'>974</div>
+								<div className='stat-text'>uczestników</div>
 							</div>
 							<div className='stat-box'>
-								<div
-									className='stat-number yellow'
-									style={{ marginTop: "47px" }}
-								>
-									69
-								</div>
-								<div
-									className='stat-text yellow'
-									style={
-										window.matchMedia(
-											"(min-width:501px) and (max-width:1000px)"
-										) === true
-											? { marginTop: "-50px" }
-											: { marginTop: "18px" }
-									}
-								>
-									warsztatów
-								</div>
+								<div className='stat-number yellow'>69</div>
+								<div className='stat-text yellow'>warsztatów</div>
 							</div>
 							<div className='stat-box'>
-								<div className='stat-number' style={{ marginTop: "47px" }}>
-									8
-								</div>
-								<div
-									className='stat-text'
-									style={
-										window.matchMedia(
-											"(min-width:501px) and (max-width:1000px)"
-										) === true
-											? { marginTop: "-50px" }
-											: { marginTop: "18px" }
-									}
-								>
-									symulacji
-								</div>
+								<div className='stat-number'>8</div>
+								<div className='stat-text'>symulacji</div>
 							</div>
 						</div>
 					</div>
