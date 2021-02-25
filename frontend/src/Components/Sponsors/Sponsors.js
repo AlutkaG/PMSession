20b30af2ce623277b992874f0e37668021589a68
AlutkaG <alagerlach@interia.pl>
2021-02-25 11:55:07 +0100

    Upgraded responsiveness

--- frontend/src/Components/Sponsors/Sponsors.js
+++ frontend/src/Components/Sponsors/Sponsors.js
@@ -14,11 +14,9 @@ const Sponsors = () => {
 		<div id='sponsors'>
 			<div className='spon-full-screen'>
 				<div className='spon-header'>Sponsorzy</div>
-				<div style={{ display: "flex", marginTop: "182px", width: "100vw" }}>
+				<div className='spon-first-row'>
 					<div>
-						<div className='spon-name' style={{ width: "50vw" }}>
-							Organizator
-						</div>
+						<div className='spon-name org'>Organizator</div>
 						<img
 							src={photo1}
 							alt=''
@@ -26,9 +24,7 @@ const Sponsors = () => {
 						/>
 					</div>
 					<div>
-						<div className='spon-name' style={{ width: "50vw" }}>
-							Sponsor strategiczny
-						</div>
+						<div className='spon-name strat'>Sponsor strategiczny</div>
 						<img
 							src={photo2}
 							alt=''
@@ -36,133 +32,34 @@ const Sponsors = () => {
 						/>
 					</div>
 				</div>
-				<div className='spon-name' style={{ marginTop: "210px" }}>
+				<div className='spon-name' style={{ marginTop: "15vh" }}>
 					Partnerzy
 				</div>
 				<img src={photo3} alt='' style={{ width: "45vw", marginTop: "10vh" }} />
-				<div className='spon-name' style={{ marginTop: "239px" }}>
+				<div className='spon-name' style={{ marginTop: "15vh" }}>
 					Media
 				</div>
 				<div className='spon-media-div'>
-					<div>
-						<img src={photo4} alt='' style={{ width: "50vw" }} />
+					<div className='image'>
+						<img src={photo4} alt='' />
 					</div>
-					<div>
-						<img src={photo5} alt='' style={{ width: "50vw" }} />
+					<div className='image'>
+						<img src={photo5} alt='' />
 					</div>
-					<div>
+					<div className='image'>
 						<img src={photo6} alt='' />
 					</div>
 				</div>
 				<div className='spon-media-div2'>
-					<div>
+					<div className='image'>
 						<img src={photo7} alt='' />
 					</div>
-					<div>
+					<div className='image'>
 						<img src={photo8} alt='' />
 					</div>
-					<div>
+					<div className='image'>
 						<img src={photo9} alt='' />
 					</div>
-				</div>
-			</div>
-			{/* mobile screen */}
-			<div className='spon-mobile-screen'>
-				<div className='spon-header'>Sponsorzy</div>
-
-				<div className='spon-name'>Organizator</div>
-				<img
-					src={photo1}
-					alt=''
-					style={{
-						width: "322px",
-						height: "198.5px",
-						marginTop: "63px",
-						marginLeft: "20px",
-						marginBottom: "20px",
-					}}
-				/>
-
-				<div className='spon-name'>Sponsor strategiczny</div>
-				<img
-					src={photo2}
-					alt=''
-					style={{
-						width: "300px",
-						height: "433px",
-						marginTop: "63px",
-						marginLeft: "20px",
-					}}
-				/>
-
-				<div className='spon-name' style={{ marginTop: "80px" }}>
-					Partnerzy
-				</div>
-				<img
-					src={photo3}
-					alt=''
-					style={{ marginTop: "80px", width: "322px", height: "78.5px" }}
-				/>
-				<div className='spon-name' style={{ marginTop: "80px" }}>
-					Media
-				</div>
-				<div>
-					<img
-						src={photo4}
-						alt=''
-						style={{ width: "322px", height: "200px", marginTop: "30px" }}
-					/>
-				</div>
-				<div>
-					<img
-						src={photo5}
-						alt=''
-						style={{
-							width: "322px",
-							height: "73px",
-							marginTop: "30px",
-						}}
-					/>
-				</div>
-				<div>
-					<img
-						src={photo6}
-						alt=''
-						style={{
-							width: "322px",
-							height: "36.5px",
-							marginTop: "50px",
-						}}
-					/>
-				</div>
-				<div>
-					<img
-						src={photo7}
-						alt=''
-						style={{ width: "322px", height: "91.5px", marginTop: "30px" }}
-					/>
-				</div>
-				<div>
-					<img
-						src={photo8}
-						alt=''
-						style={{
-							width: "322px",
-							height: "156.6px",
-							marginTop: "40px",
-						}}
-					/>
-				</div>
-				<div>
-					<img
-						src={photo9}
-						alt=''
-						style={{
-							width: "322px",
-							height: "77px",
-							marginTop: "40px",
-						}}
-					/>
 				</div>
 			</div>
 		</div>
