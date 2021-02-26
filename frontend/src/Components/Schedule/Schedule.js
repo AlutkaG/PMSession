b32a398482b0f642cd50693c2614b71150df8301
Skylade <patrykinfo1900@gmail.com>
2021-02-26 17:58:47 +0100

    minor fixes

--- frontend/src/Components/Schedule/Schedule.js
+++ frontend/src/Components/Schedule/Schedule.js
@@ -1,17 +1,21 @@
 import Programme from "./ProgrammeSection/Programme";
 import SpeakersSection from "./SpeakersSection/SpeakersSection";
 import TopWave from "./TopWaveSection/TopWave";
+import photo from "../../Assets/SzymonGornik_PMI.jpg"
+
+const speakersRows =[{title: "Lorem Ipsumowicz", subTitle:"minim veniam", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ", photoL: photo, photoR:photo},
+{title: "Lorem Ipsumowicz", subTitle:"minim veniam", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ", photoL: photo, photoR:photo},
+{title: "Lorem Ipsumowicz", subTitle:"minim veniam", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ", photoL: photo, photoR:photo}]
+
+
 
 const Schedule = () => {
     return (
         <div>
             <TopWave />
             <Programme />
-            <div className="section-title programme-title">Prelegenci</div>
-            < SpeakersSection />
-            < SpeakersSection />
-            < SpeakersSection />
             
+            < SpeakersSection speakersRows={speakersRows}/>
         </div>
     );
 };
--- frontend/src/Components/Schedule/SpeakersSection/SpeakersSection.js
+++ frontend/src/Components/Schedule/SpeakersSection/SpeakersSection.js
@@ -1,12 +1,14 @@
 import "./SpeakersSection.css";
-import photo from "../../../Assets/SzymonGornik_PMI.jpg"
 
-const speakerRow =[{title: "Lorem Ipsumowicz", subTitle:"minim veniam", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ", photoL: photo, photoR:photo}]
-const SpeakersSection = () => {
+
+const SpeakersSection = (props) => {
+    const speakersRows = props.speakersRows;
     return (
+        
         <div className="speakers-section">
-            
-            {speakerRow.map((row, index)=> {return(<div className="speaker-item">
+            <div className="section-title programme-title">Prelegenci</div>
+            {speakersRows.map((row)=> {return(
+            <div className="speaker-item">
                 <div className="photo-l">
                     <img src={row.photoL} alt=""/>
                 </div>
