4dba39bfd44a4b4ba82bf0fc57c1ac46c96334ff
Skylade <patrykinfo1900@gmail.com>
2021-02-26 15:57:17 +0100

    Refactored SpeakersSection

--- frontend/src/Components/Schedule/SpeakersSection/SpeakersSection.js
+++ frontend/src/Components/Schedule/SpeakersSection/SpeakersSection.js
@@ -1,24 +1,26 @@
 import "./SpeakersSection.css";
 import photo from "../../../Assets/SzymonGornik_PMI.jpg"
+
+const speakerRow =[{title: "Lorem Ipsumowicz", subTitle:"minim veniam", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ", photoL: photo, photoR:photo}]
 const SpeakersSection = () => {
     return (
         <div className="speakers-section">
             
-            <div className="speaker-item">
+            {speakerRow.map((row, index)=> {return(<div className="speaker-item">
                 <div className="photo-l">
-                    <img src={photo} alt=""/>
+                    <img src={row.photoL} alt=""/>
                 </div>
                 <div className="speaker-description">
-                    <div className="description-title">Lorem Ipsumowicz</div>
-                    <div className="description-sub-title">minim veniam</div>
+                    <div className="description-title">{row.title}</div>
+                    <div className="description-sub-title">{row.subTitle}</div>
                     <div className="description-content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        {row.description}
                     </div>
                 </div>
-                <div className="photo-l"><img src={photo} alt=""/></div>
-            </div>
+                <div className="photo-l"><img src={row.photoR} alt=""/></div>
+            </div>)})}
+
+            
         </div>
     );
 };
