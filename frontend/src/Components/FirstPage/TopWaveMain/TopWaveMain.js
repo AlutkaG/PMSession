2face1aee035342d5b0ea013f8793f310c802068
AlutkaG <alagerlach@interia.pl>
2021-04-25 00:34:51 +0200

    changed link

--- frontend/src/Components/FirstPage/TopWaveMain/TopWaveMain.js
+++ frontend/src/Components/FirstPage/TopWaveMain/TopWaveMain.js
@@ -19,7 +19,7 @@ const TopWaveMain = () => {
               początkujących jak i profesjonalnych project managerów.
             </div>
             <div style={{ paddingTop: "45px" }}>
-              <button className="wave-button"><a href="https://pwr-edu.zoom.us/j/95705993032" target="_blank">Link do konferencji</a></button>
+              <button className="wave-button"><a href="https://pwr-edu.zoom.us/j/92136098753" target="_blank">Link do konferencji</a></button>
             </div>
           </div>
           <div className="wave-img">
